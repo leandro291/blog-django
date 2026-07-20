e940757c03065b596a3d86f03621a8fd6f4736c5
leandro291 <lenadrito1@gmail.com>
2026-07-19 21:50:11 -0500

    feat: Agregacion de 'rest_framwerok' para generar las APIS y 'drf_spectacular' para   la generacion de documentacion Swagger

--- blog/settings.py
+++ blog/settings.py
@@ -44,7 +44,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Created
     'users',
+
+    # Installed
+    'rest_framework',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'API generativa de Blogs',
+    'DESCRIPTION': 'Api para una pagina de Blogs',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
