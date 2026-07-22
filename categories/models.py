05e900d07644c049afddbdd3ab648bee6c6aa497
leandro291 <lenadrito1@gmail.com>
2026-07-22 10:09:09 -0500

    feat: Creacion del modelo de categoria

---
+++ categories/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
