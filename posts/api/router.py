8b1c0c8bbea6f82dc9ce773d310a814633be241e
leandro291 <lenadrito1@gmail.com>
2026-07-22 11:06:16 -0500

    feat: Creacion de enrutador para posts

---
+++ posts/api/router.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+
+from posts.api.views import PostViewSet
+
+router_posts = DefaultRouter()
+
+router_posts.register('posts', PostViewSet, basename='posts')
