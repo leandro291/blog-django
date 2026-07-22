2903028b3a4508a25d52ad6f6bfe999402030888
leandro291 <lenadrito1@gmail.com>
2026-07-22 09:24:36 -0500

    feat: Creacion del enrutador para los metodos del user

--- users/api/router.py
+++ users/api/router.py
@@ -1,9 +1,14 @@
-from rest_framework.routers import DefaultRouter
-from users.api.views import UserModelViewSet
+from django.urls import path
+from users.api.views import RegisterView, UserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name="register"),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/me/', UserView.as_view(), name="me"),
+]
 
-router = DefaultRouter()
-
-router.register('users', UserModelViewSet, basename='users')
-
-urlpatterns = router.urls
