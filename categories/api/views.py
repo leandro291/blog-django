60fc12d9f701ae8afe70be4c95d56d333df0902e
leandro291 <lenadrito1@gmail.com>
2026-07-22 10:10:25 -0500

    feat: Creacion del CRUD de categorias mediante ModelViewSet

---
+++ categories/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+from categories.models import Category
+from categories.api.serializers import CategorySerializer
+from categories.api.permissions import IsAdminOrReadOnly
+
+class CategoryViewSet(ModelViewSet):
+    permission_classes = (IsAdminOrReadOnly,)
+    queryset = Category.objects.filter(published=True)
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('published', 'title')
