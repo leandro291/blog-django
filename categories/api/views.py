b58cf0b6ceda4f4bb4075152cb061af40bfa766b
leandro291 <lenadrito1@gmail.com>
2026-07-23 01:05:03 -0500

    refactor: Actualizacion de views de categoria para separar utilidades

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,14 +1,25 @@
+from categories.models import Category
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 
-from categories.models import Category
-from categories.api.serializers import CategorySerializer
+from categories.api.serializers import CategoryReadSerializer, CategoryWriteSerializer
 from categories.api.permissions import IsAdminOrReadOnly
 
 class CategoryViewSet(ModelViewSet):
     permission_classes = (IsAdminOrReadOnly,)
     queryset = Category.objects.filter(published=True)
-    serializer_class = CategorySerializer
     lookup_field = 'slug'
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('published', 'title')
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'update', 'partial_update'):
+            return CategoryWriteSerializer
+
+        return CategoryReadSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Category.objects.all()
+
+        return Category.objects.filter(published=True)
