020abe911a255de1e0b4d0ecc1f5cf1b42d262e6
leandro291 <lenadrito1@gmail.com>
2026-07-23 01:32:19 -0500

    refactor: Actualizacion del views.py

--- users/api/views.py
+++ users/api/views.py
@@ -1,14 +1,11 @@
-from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-from rest_framework.views import APIView
 from users.api.serializers import UserRegisterSerializer, UserSerializer, UserUpdateSerializer
-from users.models import User
+
 
 class RegisterView(CreateAPIView):
     serializer_class = UserRegisterSerializer
+
 
 class UserView(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
@@ -19,5 +16,4 @@ class UserView(RetrieveUpdateAPIView):
     def get_serializer_class(self):
         if self.request.method == 'PUT':
             return UserUpdateSerializer
-
         return UserSerializer
