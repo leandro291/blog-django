0cbc608c087581bb8883a346815d66889cde2c1d
leandro291 <lenadrito1@gmail.com>
2026-07-22 21:12:42 -0500

    refactor: Mejorar la leida de datos en el serializers.py

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,13 +1,17 @@
-from rest_framework.serializers import ModelSerializer
 from posts.models import Post
+from rest_framework.serializers import ModelSerializer
+
 from users.api.serializers import UserSerializer
 from categories.api.serializers import CategorySerializer
 
+class PostWriteSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'miniature', 'category', 'published', 'slug')
 
-class PostSerializer(ModelSerializer):
-
-    user = UserSerializer()
-    category = CategorySerializer()
+class PostReadSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
 
     class Meta:
         model = Post
