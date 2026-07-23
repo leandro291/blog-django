93def7aa409ae8da84e8574c095e2f1f5dc356a2
leandro291 <lenadrito1@gmail.com>
2026-07-23 01:00:45 -0500

    feat: Agregacion de url del apartado de comentario

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from categories.api.router import router_categories
 from posts.api.router import router_posts
+from comments.api.router import router_comments
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -32,5 +33,6 @@ urlpatterns = [
     path('api/', include('users.api.router'), name='user'),
     path('api/', include(router_categories.urls)),
     path('api/', include(router_posts.urls)),
+    path('api/', include(router_comments.urls)),
 
 ]
