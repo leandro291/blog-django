fbffc86d894c32f05d29b487e7c03fd7972e83d8
leandro291 <lenadrito1@gmail.com>
2026-07-22 21:16:51 -0500

    refactor: Mejorar la serializacion de leida y entrada, querset para vista de admin y user

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,13 +1,26 @@
 from rest_framework.viewsets import ModelViewSet
-from posts.api.serializers import PostSerializer
+from posts.api.serializers import PostReadSerializer, PostWriteSerializer
 from posts.models import Post
 from posts.api.permissions import IsAdminOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
 class PostViewSet(ModelViewSet):
-    queryset = Post.objects.filter(published=True)
-    serializer_class = PostSerializer
     lookup_field = 'slug'
     permission_classes = [IsAdminOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['category__slug', 'category']
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'update', 'partial_update'):
+            return PostWriteSerializer
+
+        return PostReadSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Post.objects.all().select_related('user', 'category')
+
+        return Post.objects.filter(published=True).select_related('user', 'category')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
